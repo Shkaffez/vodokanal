d7e92505b8a0a096481af4611d69de1d0f4273df
Shkaffez <shishkov-i@mail.ru>
2023-03-06 22:14:36 +0300

    finish wyUs block

--- src/components/HomeComponents/WhyUs.jsx
+++ src/components/HomeComponents/WhyUs.jsx
@@ -6,14 +6,16 @@ export default function WhyUs() {
     return (
         <div className={styles.wrapper}>
             <Image className={styles.mainImg} src="bg5.jpg" />
-            <Container className={styles.container}>
+            <Container>
                 <Card className={styles.card}>
                     <Card.Body>
-                        <Card.Title>Почему выбирают нас</Card.Title>
-                        <Card.Title>Мы профессионалы в инженерке</Card.Title>
-                        <Card.Text>
-                            «Водоканалсбыт» – крупнейшая компания по монтажу и обслуживанию систем отопления, и водоснабжения в Москве и Московской области.
-                            Уже много лет мы предоставляем высококачественное оборудование и услуги физическим и юридическим лицам, выполняя полный комплекс монтажных работ и обслуживания инженерных систем.
+                        <Card.Title className={styles.cardText1}>Почему выбирают нас</Card.Title>
+                        <Card.Title className={styles.cardText2}>Мы профессионалы в инженерке</Card.Title>
+                        <Card.Text className={styles.cardText}>
+                            «Водоканалсбыт» – крупнейшая компания по монтажу и обслуживанию систем отопления, и водоснабжения в Москве и Московской области. <br /><br />
+
+                            Уже много лет мы предоставляем высококачественное оборудование и услуги физическим и юридическим лицам, выполняя полный комплекс монтажных работ и обслуживания инженерных систем. <br /><br />
+
                             Наличие собственного производства дает условия в точности изготавливать комплектующие для любого проекта.
                         </Card.Text>
 
@@ -30,6 +32,13 @@ export default function WhyUs() {
                     </div >
                 </Card >
             </Container>
-        </div>
+            <div className={styles.miniImgWrapper}>
+                <Image className={styles.miniImg} src="a.jpg" />
+                <Image className={styles.miniImg} src="attestat-kachestva-vodokanalsbit-e1606482813996.jpg" />
+                <Image className={styles.miniImg} src="d.jpg" />
+                <Image className={styles.miniImg} src="gost-vodokanalsbit-e1606482791848.jpg" />
+                <Image className={styles.miniImg} src="strahovanie-imushcestva-vodokanalsbit-2.jpg" />
+            </div>
+        </div >
     )
 }
